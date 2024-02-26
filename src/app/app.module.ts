fbab5eaf1195fc4e8f9734564807a23328a0eb51
Willian <williamhernandez450@gmail.com>
2024-02-26 05:30:27 -0600

    Add ngrxStore and refactor counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './modules/counter/state/counter.reducer';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { LayoutModule } from './layout/layout.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LayoutModule
+    LayoutModule,
+    StoreModule.forRoot({
+      counter: counterReducer
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/counter/components/counter-buttons/counter-buttons.component.ts
+++ src/app/modules/counter/components/counter-buttons/counter-buttons.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment, reset } from '../../state/counter.actions';
 
 @Component({
   selector: 'app-counter-buttons',
@@ -7,25 +9,28 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class CounterButtonsComponent implements OnInit {
 
-  @Output() increment = new EventEmitter<void>();
-  @Output() decrement = new EventEmitter<void>();
-  @Output() reset = new EventEmitter<void>();
+  valueCounter?: number;
 
-  constructor() { }
+  constructor(
+    private store: Store<{ counter: { counter: number } }>
+  ) { }
 
   ngOnInit(): void {
+    this.store.select('counter').subscribe(data => {
+      this.valueCounter = data.counter;
+    });
   }
 
   onIncrement() {
-    this.increment.emit();
+    this.store.dispatch(increment());
   }
 
   onDecrement() {
-    this.decrement.emit();
+    if( this.valueCounter! > 0 ) this.store.dispatch(decrement());
   }
 
   onReset() {
-    this.reset.emit();
+    this.store.dispatch(reset());
   }
 
 }
